190c696d6be08baa5e7eb95b517a6cef1d37d2f7
believeshe123-coder <believeshe123@gmail.com>
2026-08-14 14:31:48 -0500

    Refactor Fireball script to use constants

--- godot-combat-sim/scripts/fireball.gd
+++ godot-combat-sim/scripts/fireball.gd
@@ -1,27 +1,19 @@
-class_name Fireball
 extends Area3D
 
-@export var speed := 18.0
-@export var damage := 20
-@export var lifetime := 3.0
-
-var _spent := false
+const SPEED = 10.0
+const DAMAGE = 20
 
 
-func _ready() -> void:
+func _ready():
 	body_entered.connect(_on_body_entered)
-	get_tree().create_timer(lifetime).timeout.connect(queue_free)
 
 
-func _physics_process(delta: float) -> void:
-	global_position += -global_transform.basis.z * speed * delta
+func _physics_process(delta):
+	global_position += -global_transform.basis.z * SPEED * delta
 
 
-func _on_body_entered(body: Node3D) -> void:
-	if _spent:
-		return
-
-	_spent = true
+func _on_body_entered(body):
 	if body.has_method("take_damage"):
-		body.take_damage(damage)
+		body.take_damage(DAMAGE)
+
 	queue_free()
